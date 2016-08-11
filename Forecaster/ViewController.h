80a1d2058eb910af4229888c253e29f9306ede8c
wiseguy16 <gergusa04@gmail.com>
2016-08-11 14:01:27 -0400

    Initial Commit

---
+++ Forecaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forecaster
+//
+//  Created by Gregory Weiss on 8/11/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
